cbf1f908cb85e75d11c5738d41f80b1970255b38
Cullen J. ONeill <3659487+cjon256@users.noreply.github.com>
2025-01-22 20:55:47 -0500

    nvim - add keymap for <leader>nc for gitsigns.next_hunk()

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -35,6 +35,15 @@ return {
       end
 
       -- Navigation
+      keymap("n", "<leader>nc", function()
+        if vim.wo.diff then
+          return "]c"
+        end
+        vim.schedule(function()
+          gs.next_hunk()
+        end)
+        return "<Ignore>"
+      end, { expr = true, desc = "Next hunk {gitsigns}" })
       keymap("n", "]c", function()
         if vim.wo.diff then
           return "]c"
