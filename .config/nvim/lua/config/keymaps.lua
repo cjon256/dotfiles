241e05b629797a9e970a919f09a741fda8adf1bb
Cullen J. ONeill <3659487+cjon256@users.noreply.github.com>
2025-06-07 20:03:18 -0400

    nvim - native MacOS keymaps for copy/paste
    https://neovide.dev/faq.html#how-can-i-use-cmd-ccmd-v-to-copy-and-paste

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -380,3 +380,13 @@ end)
 keymap("n", "<C-S-N>", function()
   require("harpoon"):list():next()
 end)
+
+-- Native Macos paste/copy for Neovide
+if vim.g.neovide then
+  vim.keymap.set('n', '<D-s>', ':w<CR>') -- Save
+  vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+  vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
+  vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
+  vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
+  vim.keymap.set('i', '<D-v>', '<C-O>"+P<CR>') -- Paste insert mode
+end
