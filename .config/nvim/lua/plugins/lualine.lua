e7a6702081e77b87d8cf07a233dd6b425ef989ec
Cullen J. ONeill <3659487+cjon256@users.noreply.github.com>
2025-07-06 01:03:49 -0400

    nvim - improved lualine with dirty_buffer count

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,16 +1,62 @@
+local function listed_buffer_count()
+  local total_buffers = 0
+  local dirty_buffers = 0
+
+  -- Loop through all buffers
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    -- We only care about listed buffers
+    if vim.bo[bufnr].buflisted then
+      total_buffers = total_buffers + 1
+      -- If it's listed, check if it's also modified
+      if vim.bo[bufnr].modified then
+        dirty_buffers = dirty_buffers + 1
+      end
+    end
+  end
+
+  if total_buffers <= 1 then
+    return ""
+  end
+
+  local total_str = string.format("[%d", total_buffers)
+
+  -- If there are dirty buffers, append that info
+  --
+  if dirty_buffers > 0 then
+    -- The '' is a pencil icon (nf-fa-pencil)
+    local dirty_str = string.format("  %d]", dirty_buffers)
+    return total_str .. dirty_str
+  end
+
+  -- Otherwise, just return the total count
+  return total_str .. "]"
+end
+
 return {
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_c, {
-        function()
-          if vim.api.nvim_get_option_value("modified", {}) then
-            return "+" -- any message or icon
-          end
-          return ""
-        end,
-      })
-    end,
+    opts = {
+      sections = {
+        lualine_c = {
+          LazyVim.lualine.root_dir(),
+          {
+            "diagnostics",
+            symbols = {
+              error = LazyVim.config.icons.diagnostics.Error,
+              warn = LazyVim.config.icons.diagnostics.Warn,
+              info = LazyVim.config.icons.diagnostics.Info,
+              hint = LazyVim.config.icons.diagnostics.Hint,
+            },
+          },
+
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { LazyVim.lualine.pretty_path({ modified_sign = "●" }), separator = "" },
+          { listed_buffer_count },
+
+          -- { "buffers", mode = 2, symbols = { modified = "●" } },
+        },
+      },
+    },
   },
 }
