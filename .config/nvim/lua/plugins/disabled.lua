c74ca6de494fc46d7a01c350c9ee4aefcfad7661
Cullen J. ONeill <3659487+cjon256@users.noreply.github.com>
2026-09-17 11:25:39 -0400

    vim - update disabled mini.pairs repo name

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -5,5 +5,5 @@ return {
   { "catppuccin", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
+  { "nvim-mini/mini.pairs", enabled = false },
 }
